f6fe16c8c515955c3542be3f933014b55dffac7c
akozyreva <anna.ko4523546@gmail.com>
2020-04-10 10:53:39 +0200

    add more check for tasks add

--- ch2-test-functions/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2-test-functions/tasks_proj/tests/func/test_api_exceptions.py
@@ -1,5 +1,6 @@
 import pytest 
 import tasks
+from tasks import Task
 
 
 @pytest.mark.smoke
@@ -16,3 +17,23 @@ def test_start_tasks_db_raises():
         tasks.start_tasks_db('some/great/path', 'mysql')
     exception_msg = excinfo.value.args[0]
     assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
+
+
+def test_add_task_with_id():
+    """ try to add task with id already set. - raise an exception """
+    with pytest.raises(ValueError):
+        tasks.add(Task('run', 'brian', True, '12345'))
+
+
+def test_add_task_with_not_string_summary():
+    """ try to add task with summary != string"""
+    with pytest.raises(ValueError):
+        tasks.add(Task(summary=12345))
+
+
+def test_add_task_owner_raises():
+    """ try to add task with owner != string"""
+    with pytest.raises(ValueError) as excinfo:
+        tasks.add(Task(summary='run', owner=12345))
+    exception_msg = excinfo.value.args[0]
+    assert exception_msg == 'task.owner must be string or None)'
